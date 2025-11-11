315a6b7e5aca41c74c85240411e1aad3e62eeae5
Abhishek-53 <abhishekvpatil1@gmail.com>
2025-11-11 16:05:22 +0530

    Backend

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -4,14 +4,28 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const db = require('./db');
 
+/*
+ * serializeUser determines which data of the user object should be stored in the session.
+ * We store just the user_id.
+ */
 passport.serializeUser((user, done) => {
   done(null, user.user_id);
 });
 
+/*
+ * deserializeUser is called on every request.
+ * It takes the user_id from the session and uses it to fetch
+ * the FULL user object from the database.
+ * This full object is then attached to req.user.
+ */
 passport.deserializeUser(async (id, done) => {
   try {
     const result = await db.query('SELECT * FROM users WHERE user_id = $1', [id]);
-    done(null, result.rows[0]);
+    if (result.rows.length > 0) {
+      done(null, result.rows[0]); // This is the full user object
+    } else {
+      done(new Error('User not found in session.'), null);
+    }
   } catch (err) {
     done(err, null);
   }
@@ -38,7 +52,7 @@ passport.use(
           return done(null, res.rows[0]);
         } else {
           res = await db.query(
-            'INSERT INTO users (google_id, email, full_name) VALUES ($1, $2, $3) RETURNING *',
+            'INSERT INTO users (google_id, email, full_name, display_name) VALUES ($1, $2, $3, $3) RETURNING *',
             [googleId, email, fullName]
           );
           return done(null, res.rows[0]);
